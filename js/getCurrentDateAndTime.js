698e4a14c44b5877a054279a46e9ab127dd0834e
Amon Brollo <amonbrollo@gmail.com>
2022-04-24 11:20:20 -0300

    Fixed program function

--- js/getCurrentDateAndTime.js
+++ js/getCurrentDateAndTime.js
@@ -1,15 +1,5 @@
-let daynames = [
-  "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
-];
-
-let months = [
-  "January", "February", "March", "April", "May", "June", "July", 
-  "August", "September", "October", "November", "December"
-];
-
-let d = new Date();
-let dayname = daynames[d.getDay()];
-let monthName = months[d.getMonth()];
-let todaysDate = `${dayname}, ${monthName} ${d.getDate()}, ${d.getFullYear()}`; 
+let todaysDate = new Date(document.lastModified).toLocaleString("en-US", {
+  hour12: true,
+});
 
 document.getElementById("lastUpdate").textContent = todaysDate;
