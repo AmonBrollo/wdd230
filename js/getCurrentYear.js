68ecbe8a4e45522861cf3368f2c60fe921257372
Amon Brollo <amonbrollo@gmail.com>
2022-04-25 17:20:54 -0300

    Fixed id name

--- js/getCurrentYear.js
+++ js/getCurrentYear.js
@@ -1,4 +1,4 @@
 const date = new Date();
 const currentYear = date.getFullYear();
 
-document.querySelector("#copyrightYear").textContent = currentYear;
+document.querySelector("#currentYear").textContent = currentYear;
