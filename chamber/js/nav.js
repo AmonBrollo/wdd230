4a2f4a2f81e68b77c0afa738c4e305a893f35b8c
Amon Brollo <amonbrollo@gmail.com>
2022-05-16 20:15:40 -0300

    ⚙ Changed previous funcitions to new functions to be able to toggle the menu

--- chamber/js/nav.js
+++ chamber/js/nav.js
@@ -1,15 +1,7 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+function toggleMenu() {
+  document.getElementById('navMenu').classList.toggle('open');
+  document.getElementById('menubutton').classList.toggle('open');
+}
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-/*** Programming Notes **************************************
-  Arrow Functions - es6 syntactically compact alternative to a regular function expression
-  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-  or https://www.w3schools.com/js/js_arrow_function.asp
-
-  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  */
+const menuButton = decument.getElementById('menuButton');
+menuButton.onclick = toggleMenu;
