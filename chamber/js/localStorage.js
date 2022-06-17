249dd8d6be78b110ddb09f4770c67bd233a262e5
Amon Brollo <amonbrollo@gmail.com>
2022-06-17 15:40:29 -0300

    🌌 Created and added code to log visits in the local storage

---
+++ chamber/js/localStorage.js
@@ -0,0 +1,37 @@
+const dayVisited = new Date();
+
+let lastDayVisited;
+let timeDifference;
+
+if (!lastDayVisited) {
+  localStorage.setItem("lastDayVisited", dayVisited);
+} else {
+  lastDayVisited = localStorage.getItem("lastDayVisited");
+  localStorage.setItem("lastDayVisited", dayVisited);
+  timeDifference = difference(lastDayVisited, dayVisited);
+}
+
+function difference(date1, date2) {
+  const date1utc = Date.UTC(
+    date1.getFullYear(),
+    date1.getMonth(),
+    date1.getDate()
+  );
+  const date2utc = Date.UTC(
+    date2.getFullYear(),
+    date2.getMonth(),
+    date2.getDate()
+  );
+  const day = 1000 * 60 * 60 * 24;
+  return (date2utc - date1utc) / day;
+}
+
+if (!timeDifference) {
+  timeDifference = 0;
+  document.getElementById("numDaysLastVisit").textContent =
+    "- Today is your first visit!";
+} else {
+  document.getElementById("numDaysLastVisit").textContent = timeDifference;
+}
+
+localStorage.setItem("numberOfDaysSinceLastVisit", timeDifference);
